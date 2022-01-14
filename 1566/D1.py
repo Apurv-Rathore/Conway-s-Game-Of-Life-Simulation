c4692b1fb7bf1130b5be2690b2585fd09ff4aae1
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 22:21:20 +0530

    Add solution for 1566D1

---
+++ 1566/D1.py
@@ -0,0 +1,37 @@
+import sys,os,io
+import math 
+from collections import defaultdict
+def ii():
+    return int(input())
+def li():
+    return list(map(int,input().split()))
+if(os.path.exists('input.txt')):
+    sys.stdin = open("input.txt","r") ; sys.stdout = open("output.txt","w") 
+# else:
+#     input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+
+
+def solve():
+    n,m = li()
+    a = li()
+    l = []
+    for i in range(m):
+        l.append([a[i] , i])
+    l = list(sorted(l,key = lambda x:x[1], reverse=True))
+    
+    l = list(sorted(l,key = lambda x:x[0], reverse=False))
+
+    # print(l)
+    fill = [0]*m 
+    ans = 0
+    for x,y in l:
+        ans += sum(fill[:y])
+        fill[y] = 1 
+    print(ans)
+
+
+t = 1
+t = ii()
+for _ in range(t):
+    solve()
+
